70440a1cbef43389f96cdc964f947ed5984e8329
robotica <robotica@labcl09.inge.itam.mx>
2019-10-18 11:48:44 -0500

    Acelerometro IIC y XBee

--- PM_01/Practica3/acelerometro/acelerometro.ino
+++ PM_01/Practica3/acelerometro/acelerometro.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 
 int estado[] = {0,0,0};
+int aux = 0;
+int valores = 0;
+
 
 int xpin = A1;                  
 int ypin = A2;                  
@@ -9,22 +12,39 @@ int zpin = A3;
 float x = 0;
 float y = 0;
 float z = 0;
+
+float refx = 0;
+float refy = 0;
+float refz = 0;
   
 //int t_muestreo = 100;
-void setup()
-{
- Serial.begin(9600);
-  analogReference(EXTERNAL);  
+void setup(){
+  Serial.begin(9600);
+  //Wire.begin();
+  //analogReference(EXTERNAL);  
   
   pinMode(xpin, INPUT);
   pinMode(ypin, INPUT);
   pinMode(zpin, INPUT);
+  refx = analogRead(xpin);
+  refy = analogRead(ypin);
+  refz = analogRead(zpin);
 }
 
 void loop()
 {
- Wire.beginTransmission(1);
+ //Wire.beginTransmission(1);
  estado[0] = analogRead(xpin); 
+ aux = (refx - estado[0]);
+ if(aux < -15) {
+  valores = 0;
+ } else {
+  if (aux > 15) {
+    valores = 2;
+  } else {
+    valores = 1;
+  }
+ }
  
     delay(1);
  
@@ -35,16 +55,19 @@ void loop()
  estado[2] = analogRead(zpin);
  
 //estado = {digitalRead(inPin1), digitalRead(inPin2)};  // read input value
-  Wire.write(estado[0]);
-  Wire.endTransmission();
- 
-  Serial.print(estado[0]);
-  Serial.print("\t");
- 
-  Serial.print(estado[1]);
-  Serial.print("\t");
-  
-  Serial.println(estado[2]);
+  //Wire.write(valores);
+  //Wire.endTransmission();
+Serial.print(valores);
+//  Serial.print(valores);
+//  Serial.print("\t");
+// 
+//  Serial.print(refx - estado[0]);
+//  Serial.print("\t");
+// 
+//  Serial.print(refy - estado[1]);
+//  Serial.print("\t");
+//  
+//  Serial.println(refz - estado[2]);
  
   //delay(t_muestreo);
 }
